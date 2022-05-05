ab8f2bd4fd0d2d4f3fcbae232c610730c1c2633f
Tomas-Parizek <parizektom@email.cz>
2022-05-05 15:38:27 +0200

    Nahrání Nimu

--- C/Jednoduchá hra/Nim.c
+++ C/Jednoduchá hra/Nim.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int zadani(int min, int max, char * hlaska)
 {
@@ -12,7 +13,58 @@ int zadani(int min, int max, char * hlaska)
     return cislo;
 }
 
+int min(int a, int b)
+{
+    if (a < b) return a;
+    return b;
+}
+
+void vyhra()
+{
+    system("clear");
+    printf("Vyhrál jsi.\n\n");
+}
+
+int zadavaPC(int celkovyPocet, int zadano)
+{
+    if (celkovyPocet >= 1 && celkovyPocet <= 3)
+        return celkovyPocet;
+    else if (celkovyPocet >= 5 && celkovyPocet <= 7)
+        return celkovyPocet-4;
+    else
+        return (4 - zadano);
+}
+
 int main()
 {
+    int pocetKamenu = zadani(10, 20, "Zadej, kolik si chceš vytáhnout kamenů: ");
+    int zadano;
 
+    system("clear");
+    // Na Windowsu system("cls");
+
+    for (;;)
+    {
+        printf("Máš %d kamenů.\n", pocetKamenu);
+        zadano = zadani(1, min(pocetKamenu, 3), "Kolik jich chceš vzít? (1-3: )");
+        pocetKamenu -= zadano;
+
+        if (pocetKamenu == 0)
+        {
+            vyhra();
+            return 0;
+        }
+
+        zadano = zadavaPC(pocetKamenu, zadano);
+        printf("Počítač bere %d kamenů.\n", zadano);
+        pocetKamenu -= zadano;
+
+        if (pocetKamenu == 0)
+        {
+            prohra(zadano);
+            return 0;
+        }
+
+        printf("\n\n\n");
+    }
 }
